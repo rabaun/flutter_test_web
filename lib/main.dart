fb7d2b586bf4c5b6c84a465a1e0385cd769ea147
andrew_swyatenco <swyatenco@mail.ru>
2022-03-22 20:30:46 +0300

    add new commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //Set the fit size (fill in the screen size of the device in the design) If the design is based on the size of the iPhone6 ​​(iPhone6 ​​750*1334)
     return ScreenUtilInit(
       designSize: Size(360, 690),
       minTextAdapt: true,
