b105c2e1d7ed68a255589e588066cf625f7b0f8a
andrew_swyatenco <swyatenco@mail.ru>
2022-05-02 02:14:36 +0300

    add new

--- lib/widget/pictureslider.dart
+++ lib/widget/pictureslider.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PrefetchImageDemo extends StatefulWidget {
   @override
